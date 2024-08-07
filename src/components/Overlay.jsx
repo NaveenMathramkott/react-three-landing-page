9fac56f10423c84a88aa68a01cf55ed3e6f178aa
naveenmathramkott <naveenmathramkott@gmail.com>
2024-08-07 23:43:22 +0530

    bugfix null check added

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -16,139 +16,141 @@ const Overlay = () => {
   const tl = gsap.timeline();
 
   useLayoutEffect(() => {
-    new ScrollTrigger({});
-    tl.to(scene.position, {
-      x: 2,
-      y: 0,
-      z: 0,
-    })
-      .to(scene.rotation, {
-        x: 0,
-        y: -0.3,
+    if (jdOne.current && jdTwo.current && jdThree.current) {
+      new ScrollTrigger({});
+      tl.to(scene.position, {
+        x: 2,
+        y: 0,
         z: 0,
       })
-      .to(camera.position, {
-        x: -4.8,
-        y: 3.8,
-        z: 12,
-        scrollTrigger: {
-          trigger: ".second-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
-      .to(scene.position, {
-        x: -3,
-        y: 1,
-        z: 2,
-        scrollTrigger: {
-          trigger: ".second-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
-      .to(scene.rotation, {
-        x: 0,
-        y: Math.PI * 2,
-        z: 0,
-        scrollTrigger: {
-          trigger: ".second-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
-      .to(jdTwo.current.rotation, {
-        x: 0,
-        y: Math.PI * 2,
-        z: 0,
-        scrollTrigger: {
-          trigger: ".second-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
-      .to(jdThree.current.rotation, {
-        x: 0,
-        y: Math.PI * 2,
-        z: 0,
-        scrollTrigger: {
-          trigger: ".second-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
+        .to(scene.rotation, {
+          x: 0,
+          y: -0.3,
+          z: 0,
+        })
+        .to(camera.position, {
+          x: -4.8,
+          y: 3.8,
+          z: 12,
+          scrollTrigger: {
+            trigger: ".second-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(scene.position, {
+          x: -3,
+          y: 1,
+          z: 2,
+          scrollTrigger: {
+            trigger: ".second-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(scene.rotation, {
+          x: 0,
+          y: Math.PI * 2,
+          z: 0,
+          scrollTrigger: {
+            trigger: ".second-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(jdTwo.current.rotation, {
+          x: 0,
+          y: Math.PI * 2,
+          z: 0,
+          scrollTrigger: {
+            trigger: ".second-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(jdThree.current.rotation, {
+          x: 0,
+          y: Math.PI * 2,
+          z: 0,
+          scrollTrigger: {
+            trigger: ".second-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
 
-      .to(camera.position, {
-        x: 2,
-        y: 0,
-        z: 10,
-        scrollTrigger: {
-          trigger: ".third-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
-      .to(scene.position, {
-        x: 2,
-        y: 0,
-        z: 0,
-        scrollTrigger: {
-          trigger: ".third-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
-      .to(jdOne.current.rotation, {
-        x: 0,
-        y: Math.PI * 2,
-        z: 0,
-        scrollTrigger: {
-          trigger: ".third-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
-      .to(jdTwo.current.rotation, {
-        x: 0,
-        y: 0,
-        z: 0,
-        scrollTrigger: {
-          trigger: ".third-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      })
-      .to(jdThree.current.rotation, {
-        x: 0,
-        y: 0,
-        z: 0,
-        scrollTrigger: {
-          trigger: ".third-section",
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          immediateRender: false,
-        },
-      });
-  }, []);
+        .to(camera.position, {
+          x: 2,
+          y: 0,
+          z: 10,
+          scrollTrigger: {
+            trigger: ".third-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(scene.position, {
+          x: 2,
+          y: 0,
+          z: 0,
+          scrollTrigger: {
+            trigger: ".third-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(jdOne.current.rotation, {
+          x: 0,
+          y: Math.PI * 2,
+          z: 0,
+          scrollTrigger: {
+            trigger: ".third-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(jdTwo.current.rotation, {
+          x: 0,
+          y: 0,
+          z: 0,
+          scrollTrigger: {
+            trigger: ".third-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        })
+        .to(jdThree.current.rotation, {
+          x: 0,
+          y: 0,
+          z: 0,
+          scrollTrigger: {
+            trigger: ".third-section",
+            start: "top bottom",
+            end: "top top",
+            scrub: true,
+            immediateRender: false,
+          },
+        });
+    }
+  }, [jdOne, jdTwo, jdThree, scene, camera]);
 
   return (
     <Suspense fallback={<Loading />}>
